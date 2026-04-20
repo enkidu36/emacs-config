27e829603f3dde34f043f27e0a1df4a896b9cd50
Joseph Savoie <enkidu36@yahoo.com>
2026-04-19 20:19:21 -0500

    update init.el

--- init.el
+++ init.el
@@ -4,12 +4,15 @@
 (setq inhibit-startup-message t)
 (setq inhibit-splash-screen t)
 
-(setq default-directory "~/project/")
+(setq default-directory "~/org/")
 (scroll-bar-mode -1)        ; Disable visible scrollbar
 (tool-bar-mode -1)          ; Disable the toolbar
 (tooltip-mode -1)           ; Disable tooltips
-(set-fringe-mode 10)        ; Give some breathing room
-(menu-bar-mode t)            ; Disable the menu bar
+(set-fringe-mode 5)         ; Give some breathing room
+(menu-bar-mode t)           ; Disable the menu bar
+(blink-cursor-mode 0)       ; Disable blinking cursor
+(global-hl-line-mode 1)     ; Highlight line where the cursor is
+(transient-mark-mode 1)     ; Highlights the "mark" region
 
 ;; Set up the visible bell
 (setq visible-bell t)
@@ -21,6 +24,11 @@
 ;; For MacOS command key is control. Move Spotlight function key to
 ;; Mac control key.
 (setq mac-command-modifier 'control)
+;; (setq mac-control-modifier 'super)
+
+(set-face-attribute 'default nil :height 200)
+
+(global-set-key (kbd "C-x C-b") #'ibuffer)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;; Calendar
@@ -33,17 +41,7 @@
 ;;(display-time)
 ;;(add-hook 'diary-hook 'appt-make-list)
 ;;(diary 0)
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;;;;; MODE LINE
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-;; Disable the splash screen (to enable it agin, replace the t with 0)
-(setq inhibit-splash-screen 0)
-
-
-;; Enable transient mark mode
-(transient-mark-mode 1)
+(setq org-agenda-include-diary t)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;; ORG-MODE
@@ -53,16 +51,53 @@
 ;; (add-to-list 'auto-mode-alist '("\\.org$" . org-mode))
 ;; The above is the default in recent emacsen
 
+(setq org-agenda-custom-commands
+      '(("w" "Waiting Tasks"
+         ((todo "WAIT"
+           ((org-agenda-overriding-header "Tasks Waiting")))))))
+
 (setq org-todo-keywords
-      '((sequence "TODO(t)" "IN PROGRESS(i)" "|" "DONE(d)")
-        (sequence "REPORT(r)" "BUG(b)" "KNOWNCAUSE(k)" "|" "FIXED(f)")
-        (sequence "|" "CANCELED(c)")))
+      '((sequence "TODO(t)" "NEXT(n)" "IN PROGRESS(i)" "|" "DONE(d)")
+	(sequence "PLAN(p)" "REVIEW(v)" "WAIT(w@/!)" "HOLD(h)" "|" "COMPLETED(c)" "CANC(k@)")))
 
 (setq org-default-notes-file (concat org-directory "/notes.org"))
 
 (global-set-key (kbd "C-c a") #'org-agenda)
 (global-set-key (kbd "C-c l") #'org-store-link)
 (global-set-key (kbd "C-c c") #'org-capture)
+
+(setq org-capture-templates
+       '(("t" "Table Entry" table-line (file+headline "~/org/cycling/fit-utils.org" "Weight & Energy Tracker")
+         "| %U | %^{Lbs} | 35 | %^{Fat%} | 243 | | | |":table-line-pos "II+1" :immediate-finish t)
+
+	 ("l" "Todo" entry (file+headline "~/org/inbox.org" "Story Board")
+              "* TODO %U -  %?" :prepend t)
+
+	 ("w" "Weekly Cycling Plan" entry
+          (file+headline "~/org/cycling/cycling.org" "Weekly Plans")
+          "* Week of %^t\n:PROPERTIES:\n:GOAL: 3.0 W/kg\n:END:\n\n** Monday\n- Lift (heavy)\n- Optional 30–45 min Z2\n\n** Tuesday\n- Threshold session\n\n** Wednesday\n- Z2 aerobic\n\n** Thursday\n- Punch / VO2 session\n\n** Friday\n- Light lift\n\n** Saturday\n- Race or simulation\n\n** Sunday\n- 3 hr aerobic\n\nNotes:\n- Weight:\n- Fatigue:\n- Adjustments:\n")
+
+        ("d" "Daily Workout Log" entry
+         (file+headline "~/org/cycling/cycling.org" "Workout Log")
+         "* %U - %^{Workout Type}\n:PROPERTIES:\n:TSS: %^{TSS}\n:WEIGHT: %^{Weight}\n:END:\n\n** Plan\n%?\n\n** Actual\n\n** Notes\n- RPE:\n- Recovery:\n- Nutrition:\n")
+
+        ("p" "Phase Planning Block" entry
+         (file+headline "~/org/cycling/cycling.org" "Season Plan")
+         "* %^{Phase Name}\n:PROPERTIES:\n:START: %^t\n:END: %^t\n:END:\n\n** Objectives\n- \n\n** Focus Workouts\n- \n\n** Weight Target\n- \n\n** Notes\n%?\n")
+
+        ("c" "Weight Check-in" entry
+         (file+headline "~/org/cycling/cycling.org" "Weight Tracking")
+         "* %U\n- Weight: %^{Weight} lb\n- Weekly Trend:\n- Notes:\n")))
+
+(setq org-mobile-directory "~/Library/Mobile Documents/iCloud~com~mobileorg~mobileorg/Documents")
+
+(org-babel-do-load-languages
+ 'org-babel-load-languages
+ '((python . t)
+   (R . t)
+   (shell . t)))
+
+(setq org-babel-python-command "python3") ; Replace "python3" with your executable path
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;; ORG-AGENDA
@@ -81,6 +116,11 @@
   :custom
   (org-roam-directory "~/org-roam")
   (org-roam-completion-everywhere t)
+  ;; (org-roam-capture-template
+  ;;  '(("d" "default" plain
+  ;;     "%?" 
+  ;;     :if-new (file+head "%<%Y%m%d%H%M%S> - %{slug}.org" "#+titlfe: %{title}\n")
+  ;;     :unnarrowed t)))
   :bind (("C-c n l" . org-roam-buffer-toggle)
 	 ("C-c n f" . org-roam-node-find)
 	 ("C-c n i" . org-roam-node-insert)
@@ -91,15 +131,15 @@
 
 (setq org-roam-node-display-template
       (concat "${title:*} "
-	      (propertize "${tags:10}" 'face 'org-tag)))
+	      (propertize "${tags:10}" 'face 'org-tag) ""))
 
+(setq org-roam-dailies-directory "daily/")
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;;;;  THEME 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; Set global font size
-(set-face-attribute 'default nil :height 160)
-
+;; (setq org-roam-dailies-capture-templates
+;;            '(("d" "default" entry
+;;               "* %?"
+;;               :target (file+head "%<%Y-%m-%d>.org"
+;;                                  "#+title: %<%Y-%m-%d>\n Hello world"))))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;;; Packages
@@ -152,6 +192,39 @@
   :config
   (ivy-mode 1))
 
+;; (use-package python
+;;   :ensure nil
+;;   :hook (python-mode . eglot-hover-mode) ; Use built-in python-mode with eglot
+;;   :config
+;;   (setq python-shell-interpreter "python3") ; Set default python interpreter
+;;   (setq explicit-shell-file-name "/bin/bash") ; Use a proper shell for shell commands
+;;   )
+
+;; (use-package eglot
+;;   :ensure t
+;;   :hook (python-mode . eglot-mode) ; Activate eglot for python files
+;;   :config
+;;   ;; Optional: Enable formatting on save (requires a formatter like ruff-lsp)
+;;   (add-hook 'eglot-managed-mode-hook
+;;             (lambda ()
+;;               (add-hook 'before-save-hook #'eglot-format-buffer nil t))))
+
+;; (use-package pyvenv
+;;   :ensure t
+;;   :config
+;;   (pyvenv-mode 1)) ; Enable pyvenv global mode
+
+(use-package company
+  :ensure t
+  :hook (after-init . global-company-mode)
+  :config
+  (setq company-minimum-prefix-length 1) ; Start completion after 1 character
+  (setq company-idle-delay 0.1) ; Short delay for completion popup
+  )
+
+
+(setq split-height-threshold nil)
+(setq split-width-threshold 160)
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;;; Clojure-lsp
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
@@ -246,12 +319,6 @@
  '(mode-line ((t (:background "gray32" :foreground "gray64" :box nil :height 1.0))))
  '(mode-line-buffer-id ((t (:foreground "gray78" :weight bold))))
  '(mode-line-inactive ((t (:background "gray24" :foreground "gray64" :box nil))))
- '(org-date ((t (:foreground "#cc8800" :slant oblique))))
- '(org-level-2 ((t (:inherit outline-2 :extend nil :height 1.2))))
- '(org-level-3 ((t (:inherit outline-3 :extend nil :height 1.05))))
- '(org-todo ((t (:distant-foreground "black" :foreground "#87875f" :weight bold))))
- '(org-verse ((t (:inherit org-block))))
- '(outline-1 ((t (:extend t :foreground "#87AFD7" :underline t :slant oblique :weight bold :height 1.5))))
  '(secondary-selection ((t (:extend t :background "#656858")))))
 
 (defun efs/org-mode-setup ()
@@ -267,16 +334,16 @@
                           '(("^ *\\([-]\\) "
                              (0 (prog1 () (compose-region (match-beginning 1) (match-end 1) "•"))))))
 
-;;   ;; Set faces for heading levels
-;;   (dolist (face '((org-level-1 . 1.2)
-;;                   (org-level-2 . 1.1)
-;;                   (org-level-3 . 1.05)
-;;                   (org-level-4 . 1.0)
-;;                   (org-level-5 . 1.1)
-;;                   (org-level-6 . 1.1)
-;;                   (org-level-7 . 1.1)
-;;                   (org-level-8 . 1.1)))
-;;     )
+  ;; Set faces for heading levels
+  (dolist (face '((org-level-1 . 1.2)
+                  (org-level-2 . 1.1)
+                  (org-level-3 . 1.05)
+                  (org-level-4 . 1.0)
+                  (org-level-5 . 1.1)
+                  (org-level-6 . 1.1)
+                  (org-level-7 . 1.1)
+                  (org-level-8 . 1.1)))
+    )
 
   ;; Ensure that anything that should be fixed-pitch in Org files appears that way
   (set-face-attribute 'org-block nil :foreground nil :inherit 'fixed-pitch)
@@ -320,14 +387,16 @@
  '(custom-enabled-themes '(doom-sourcerer))
  '(custom-safe-themes
    '("de241e13dde3a1514fe767be7881e28175fa9f88238b379c191562c207a3e16f" "88f7ee5594021c60a4a6a1c275614103de8c1435d6d08cc58882f920e0cec65e" "4fdbed4aa8bcb199d7f6a643886bac51178d1705b9b354ef3dd82d4ec48072d2" "0d0936adf23bba16569c73876991168d0aed969d1e095c3f68d61f87dd7bab9a" "9dccdccfeb236623d5c7cf0250a92308cf307afde4ebdaf173b59e8bbbae1828" "5f128efd37c6a87cd4ad8e8b7f2afaba425425524a68133ac0efd87291d05874" "2078837f21ac3b0cc84167306fa1058e3199bbd12b6d5b56e3777a4125ff6851" "93011fe35859772a6766df8a4be817add8bfe105246173206478a0706f88b33d" "0c83e0b50946e39e237769ad368a08f2cd1c854ccbcd1a01d39fdce4d6f86478" "f64189544da6f16bab285747d04a92bd57c7e7813d8c24c30f382f087d460a33" "7964b513f8a2bb14803e717e0ac0123f100fb92160dcf4a467f530868ebaae3e" "89d9dc6f4e9a024737fb8840259c5dd0a140fd440f5ed17b596be43a05d62e67" "29b4f767c48da68f8f3c2bbf0dde2be58e4ed9c97e685af5a7ab7844f0d08b8b" "e1f4f0158cd5a01a9d96f1f7cdcca8d6724d7d33267623cc433fe1c196848554" "013728cb445c73763d13e39c0e3fd52c06eefe3fbd173a766bfd29c6d040f100" "9f297216c88ca3f47e5f10f8bd884ab24ac5bc9d884f0f23589b0a46a608fe14" "5b9a45080feaedc7820894ebbfe4f8251e13b66654ac4394cb416fef9fdca789" "c5878086e65614424a84ad5c758b07e9edcf4c513e08a1c5b1533f313d1b17f1" "00cec71d41047ebabeb310a325c365d5bc4b7fab0a681a2a108d32fb161b4006" "3cdd0a96236a9db4e903c01cb45c0c111eb1492313a65790adb894f9f1a33b2d" "e87fd8e24e82eb94d63b1a9c79abc8161d25de9f2f13b64014d3bf4b8db05e9a" "f5f80dd6588e59cfc3ce2f11568ff8296717a938edd448a947f9823a4e282b66" "a9abd706a4183711ffcca0d6da3808ec0f59be0e8336868669dc3b10381afb6f" "8d8207a39e18e2cc95ebddf62f841442d36fcba01a2a9451773d4ed30b632443" "2721b06afaf1769ef63f942bf3e977f208f517b187f2526f0e57c1bd4a000350" "da75eceab6bea9298e04ce5b4b07349f8c02da305734f7c0c8c6af7b5eaa9738" "f053f92735d6d238461da8512b9c071a5ce3b9d972501f7a5e6682a90bf29725" "ff24d14f5f7d355f47d53fd016565ed128bf3af30eb7ce8cae307ee4fe7f3fd0" "dc8285f7f4d86c0aebf1ea4b448842a6868553eded6f71d1de52f3dcbc960039" "c8b3d9364302b16318e0f231981e94cbe4806cb5cde5732c3e5c3e05e1472434" "38c0c668d8ac3841cb9608522ca116067177c92feeabc6f002a27249976d7434" "3de5c795291a145452aeb961b1151e63ef1cb9565e3cdbd10521582b5fd02e9a" "1f292969fc19ba45fbc6542ed54e58ab5ad3dbe41b70d8cb2d1f85c22d07e518" "162201cf5b5899938cfaec99c8cb35a2f1bf0775fc9ccbf5e63130a1ea217213" "9e1cf0f16477d0da814691c1b9add22d7cb34e0bb3334db7822424a449d20078" "a9eeab09d61fef94084a95f82557e147d9630fbbb82a837f971f83e66e21e5ad" "dfb1c8b5bfa040b042b4ef660d0aab48ef2e89ee719a1f24a4629a0c5ed769e8" "680f62b751481cc5b5b44aeab824e5683cf13792c006aeba1c25ce2d89826426" "a44e2d1636a0114c5e407a748841f6723ed442dc3a0ed086542dc71b92a87aee" "b1acc21dcb556407306eccd73f90eb7d69664380483b18496d9c5ccc5968ab43" "dccf4a8f1aaf5f24d2ab63af1aa75fd9d535c83377f8e26380162e888be0c6a9" "c517e98fa036a0c21af481aadd2bdd6f44495be3d4ac2ce9d69201fcb2578533" "e69661aa4d47e1581af25c58e9cc0ca762dc0e88e06f50e208039b38a40eddfc" "7e377879cbd60c66b88e51fad480b3ab18d60847f31c435f15f5df18bdb18184" "ffafb0e9f63935183713b204c11d22225008559fa62133a69848835f4f4a758c" "4b6cc3b60871e2f4f9a026a5c86df27905fb1b0e96277ff18a76a39ca53b82e1" "56044c5a9cc45b6ec45c0eb28df100d3f0a576f18eef33ff8ff5d32bac2d9700" "a9dc7790550dcdb88a23d9f81cc0333490529a20e160a8599a6ceaf1104192b5" "6adeb971e4d5fe32bee0d5b1302bc0dfd70d4b42bad61e1c346599a6dc9569b5" "9013233028d9798f901e5e8efb31841c24c12444d3b6e92580080505d56fd392" "75b2a02e1e0313742f548d43003fcdc45106553af7283fb5fad74359e07fe0e2" "8d3ef5ff6273f2a552152c7febc40eabca26bae05bd12bc85062e2dc224cde9a" "13096a9a6e75c7330c1bc500f30a8f4407bd618431c94aeab55c9855731a95e1" "8b148cf8154d34917dfc794b5d0fe65f21e9155977a36a5985f89c09a9669aa0" "37b6695bae243145fa2dfb41440c204cd22833c25cd1993b0f258905b9e65577" "be84a2e5c70f991051d4aaf0f049fa11c172e5d784727e0b525565bb1533ec78" "e14884c30d875c64f6a9cdd68fe87ef94385550cab4890182197b95d53a7cf40" "b9761a2e568bee658e0ff723dd620d844172943eb5ec4053e2b199c59e0bcc22" "e3daa8f18440301f3e54f2093fe15f4fe951986a8628e98dcd781efbec7a46f2" "bf948e3f55a8cd1f420373410911d0a50be5a04a8886cabe8d8e471ad8fdba8e" "6f96a9ece5fdd0d3e04daea6aa63e13be26b48717820aa7b5889c602764cf23a" "3fe1ebb870cc8a28e69763dde7b08c0f6b7e71cc310ffc3394622e5df6e4f0da" "b54376ec363568656d54578d28b95382854f62b74c32077821fdfd604268616a" "9d29a302302cce971d988eb51bd17c1d2be6cd68305710446f658958c0640f68" "df6dfd55673f40364b1970440f0b0cb8ba7149282cf415b81aaad2d98b0f0290" "f4d1b183465f2d29b7a2e9dbe87ccc20598e79738e5d29fc52ec8fb8c576fcfd" "8c7e832be864674c220f9a9361c851917a93f921fedb7717b1b5ece47690c098" "aec7b55f2a13307a55517fdf08438863d694550565dee23181d2ebd973ebd6b8" "c1d5759fcb18b20fd95357dcd63ff90780283b14023422765d531330a3d3cec2" "70e7f094987e3c6226c54078dd986e11cab7246ea1c9e58a9907afa90f3c10c9" "02d422e5b99f54bd4516d4157060b874d14552fe613ea7047c4a5cfa1288cf4f" "631c52620e2953e744f2b56d102eae503017047fb43d65ce028e88ef5846ea3b" "014cb63097fc7dbda3edf53eb09802237961cbb4c9e9abd705f23b86511b0a69" "10e5d4cc0f67ed5cafac0f4252093d2119ee8b8cb449e7053273453c1a1eb7cc" "e4a702e262c3e3501dfe25091621fe12cd63c7845221687e36a79e17cf3a67e0" "443e2c3c4dd44510f0ea8247b438e834188dc1c6fb80785d83ad3628eadf9294" "badd1a5e20bd0c29f4fe863f3b480992c65ef1fa63951f59aa5d6b129a3f9c4c" default))
+ '(dired-listing-switches "-al")
  '(initial-frame-alist '((fullscreen . maximized)))
  '(lsp-headerline-breadcrumb-enable nil)
  '(lsp-headerline-breadcrumb-enable-symbol-numbers t)
  '(org-agenda-files
-   '("/Users/joseph.savoie/org/personal.org" "/Users/joseph.savoie/org/design.org"))
+   '("/Users/joseph.savoie/org/inbox.org" "/Users/joseph.savoie/org/cycling/fit-utils.org" "/Users/joseph.savoie/org/cycling/cycling.org"))
  '(org-cycle-separator-lines 1)
  '(package-selected-packages
-   '(org-roam valign clojure-mode lsp-mode cider lsp-treemacs flycheck company)))
+   '(jupyter eglot pyvenv speed-type vertico org-roam valign clojure-mode lsp-mode cider lsp-treemacs flycheck company))
+ '(send-mail-function 'mailclient-send-it))
 (put 'downcase-region 'disabled nil)
 
 
